a3f1a6ac4075a6a07ce918525defcabca8e85a44
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2023-09-27 14:50:45 +0200

    BookCard size adjusted with container

--- front-end/src/components/pages/BookPage.jsx
+++ front-end/src/components/pages/BookPage.jsx
@@ -1,4 +1,5 @@
 import bookCover from "../../assets/king-cover.jpg"
+import { Link } from "react-router-dom"
 
 export default function BookPage ({id}) {
     return (
@@ -8,8 +9,10 @@ export default function BookPage ({id}) {
                     <img src={bookCover} alt="The Shining" className="h-[500px]" />
                     <div className="book-info">
                         <h1 className="text-5xl font-bold">The Shining</h1>
-                        <p className="italic py-4">Stephen King</p>
-                        <p>Jack Torrance (Jack Nicholson) becomes winter caretaker at the isolated Overlook Hotel in Colorado, hoping to cure his writer's block. He settles in along with his wife, Wendy (Shelley Duvall), and his son, Danny (Danny Lloyd), who is plagued by psychic premonitions. <br /> As Jack's writing goes nowhere and Danny's visions become more disturbing, Jack discovers the hotel's dark secrets and begins to unravel into a homicidal maniac hell-bent on terrorizing his family.</p>
+                        <p className="italic py-4"> <Link to={`/results?stephen-king`}>Stephen King</Link>, 1977</p>
+                        <p>
+                            Jack Torrance (Jack Nicholson) becomes winter caretaker at the isolated Overlook Hotel in Colorado, hoping to cure his writer's block. He settles in along with his wife, Wendy (Shelley Duvall), and his son, Danny (Danny Lloyd), who is plagued by psychic premonitions. <br /> As Jack's writing goes nowhere and Danny's visions become more disturbing, Jack discovers the hotel's dark secrets and begins to unravel into a homicidal maniac hell-bent on terrorizing his family.
+                        </p>
                         <p className="pt-5">Genre: Thriller</p>
                     </div>
                 </div>
