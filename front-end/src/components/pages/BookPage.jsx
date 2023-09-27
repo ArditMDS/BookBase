831411c77466e62757cdfde4f1a8e1c355792dda
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2023-09-27 13:06:29 +0200

    BookCard interface completed

--- front-end/src/components/pages/BookPage.jsx
+++ front-end/src/components/pages/BookPage.jsx
@@ -1,5 +1,19 @@
+import bookCover from "../../assets/king-cover.jpg"
+
 export default function BookPage ({id}) {
     return (
-        <h1>Here is the book component</h1>
+        <main>
+            <div className="container py-16">
+                <div className="book-wrapper flex flex-col md:flex-row items-start md:items-center gap-10">
+                    <img src={bookCover} alt="The Shining" className="h-[500px]" />
+                    <div className="book-info">
+                        <h1 className="text-5xl font-bold">The Shining</h1>
+                        <p className="italic py-4">Stephen King</p>
+                        <p>Jack Torrance (Jack Nicholson) becomes winter caretaker at the isolated Overlook Hotel in Colorado, hoping to cure his writer's block. He settles in along with his wife, Wendy (Shelley Duvall), and his son, Danny (Danny Lloyd), who is plagued by psychic premonitions. <br /> As Jack's writing goes nowhere and Danny's visions become more disturbing, Jack discovers the hotel's dark secrets and begins to unravel into a homicidal maniac hell-bent on terrorizing his family.</p>
+                        <p className="pt-5">Genre: Thriller</p>
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
